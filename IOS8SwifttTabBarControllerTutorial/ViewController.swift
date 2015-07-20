0bcf5b9da2614ffa8ff5197a59e70d55c3689c3a
noprom <tyee.noprom@qq.com>
2015-07-20 22:22:58 +0800

    Initial Commit

---
+++ IOS8SwifttTabBarControllerTutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IOS8SwifttTabBarControllerTutorial
+//
+//  Created by noprom on 15/7/20.
+//  Copyright (c) 2015年 noprom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
